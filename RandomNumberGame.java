186f2e4d8990f6097f5cdc7391b62747ce64bdf6
almufrath2006-del <almufrath2006@gmail.com>
2026-06-12 14:34:39 +0530

    file RandomNumberGame.java created

---
+++ RandomNumberGame.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class RandomNumberGame {
+    public static void main(String[] args) {
+        Scanner muf=new Scanner(System.in);
+        Random r=new Random();
+        System.out.println("Enter number.");
+        int rand=r.nextInt(10);
+        int b=0;
+        int a=muf.nextInt();
+        while (a!=rand) {
+            if (a<rand) {
+                System.out.println("Less.");
+                a=muf.nextInt(); b++;
+            }
+            else if (a>rand) {
+                System.out.println("greater");
+                a=muf.nextInt();b++;
+            }
+        }
+        if (a==rand) {
+            b++;
+            if(b==1){
+            System.out.println("single attempts "+"brillient");
+            }
+            else if(b>1&&b<=5){
+                System.out.println(b+" attempts "+"very good");
+            }
+            else{
+                System.out.println(b+" attempts "+"good");
+            }
+        }
+    }
+}
+
